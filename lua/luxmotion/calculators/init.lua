011c3e940ade5ea459a945c79838246919f33caf
josstei <48696594+josstei@users.noreply.github.com>
2026-01-26 00:06:09 -0500

    feat: add calculator index module (dependencies pending)

---
+++ lua/luxmotion/calculators/init.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.basic = require("luxmotion.calculators.basic")
+M.word = require("luxmotion.calculators.word")
+M.find = require("luxmotion.calculators.find")
+M.text_object = require("luxmotion.calculators.text_object")
+M.line = require("luxmotion.calculators.line")
+M.search = require("luxmotion.calculators.search")
+M.scroll = require("luxmotion.calculators.scroll")
+
+return M
